e34792e8ea448d41ef16eb9c639c1ea31e0673ab
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2023-01-03 18:15:45 +0330

    Update Models

--- LinkShortener/Models/Script.cs
+++ LinkShortener/Models/Script.cs
@@ -3,18 +3,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LinkShortener.Models
 {
-    public class ShortUrl
+    public class Script
     {
         [Key]
-        public int LinkId { get; set; }
+        public int ScriptId { get; set; }
 
         [Required]
         public int UserId { get; set; }
 
-        [Required(ErrorMessage = "وارد کردن لینک اجباری است")]
-        [Display(Name = "لینک ورودی")]
-        [DataType(DataType.Url, ErrorMessage = "لطفا فرمت لینک کامل باشد")]
-        public string Link { get; set; }
+        [Required(ErrorMessage = "وارد کردن {0} اجباری است")]
+        [Display(Name = "اسکریپت")]
+        public string script { get; set; }
 
         public string ShortLink { get; set; }
 
@@ -22,7 +21,8 @@ namespace LinkShortener.Models
 
         [Required]
         public DateTime CreateDate { get; set; }
-
+    
         public User User { get; set; }
+
     }
 }
