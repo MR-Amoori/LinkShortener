5464bd334787030dc066b4126e2640941b9c2977
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2023-01-05 03:10:02 +0330

    add migration

---
+++ LinkShortener/Migrations/20230104233929_update_tbl_script.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LinkShortener.Migrations
+{
+    public partial class update_tbl_script : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Customer",
+                table: "Scripts",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "RegisterDate",
+                value: new DateTime(2023, 1, 5, 3, 9, 29, 202, DateTimeKind.Local).AddTicks(4724));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Customer",
+                table: "Scripts");
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "RegisterDate",
+                value: new DateTime(2023, 1, 3, 18, 30, 0, 5, DateTimeKind.Local).AddTicks(5869));
+        }
+    }
+}
